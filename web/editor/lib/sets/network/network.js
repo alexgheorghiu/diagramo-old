54a6bf51fc12d8e9a4aa34cfbf1393a4bbada570
Alex <alex@scriptoid.com>
2012-01-04 14:52:04 +0200

    pda and phone images added

--- web/editor/lib/sets/network/network.js
+++ web/editor/lib/sets/network/network.js
@@ -9,6 +9,8 @@ figureSets["network"] = [
     {figureFunction: "Laptop", image: "8_laptop.png"},
     {figureFunction: "Desktop", image: "9_desktop.png"},
     {figureFunction: "Lock", image: "10_lock.png"},
+    {figureFunction: "PDA", image: "11_pda.png"},
+    {figureFunction: "Phone", image: "12_phone.png"},
     {figureFunction: "Printer", image: "13_printer.png"}
 ]
 
@@ -390,6 +392,88 @@ function figure_Lock(x, y){
     
     //Image
     var url = "/editor/lib/sets/network/10_lock.svg";
+    
+    var ifig = new ImageFrame(url, x, y, true, imageWidth, imageHeight);
+    ifig.debug = true;
+    f.addPrimitive(ifig);    
+    
+    //Text
+    f.properties.push(new BuilderProperty('Text', 'primitives.1.str', BuilderProperty.TYPE_TEXT));
+    f.properties.push(new BuilderProperty('Text Size', 'primitives.1.size', BuilderProperty.TYPE_TEXT_FONT_SIZE));
+    f.properties.push(new BuilderProperty('Font', 'primitives.1.font', BuilderProperty.TYPE_TEXT_FONT_FAMILY));
+    f.properties.push(new BuilderProperty('Alignment', 'primitives.1.align', BuilderProperty.TYPE_TEXT_FONT_ALIGNMENT));
+    f.properties.push(new BuilderProperty('Text Color', 'primitives.1.style.fillStyle', BuilderProperty.TYPE_COLOR));
+    
+    var t2 = new Text(figure_defaultFigureTextStr, x, y + imageHeight/2 + 5, figure_defaultFigureTextFont, figure_defaultFigureTextSize);
+    t2.style.fillStyle = figure_defaultFillTextStyle;
+    f.addPrimitive(t2);
+    
+    //Connection Points
+    
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x + imageWidth/2, y), ConnectionPoint.TYPE_FIGURE);
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x - imageWidth/2, y), ConnectionPoint.TYPE_FIGURE);
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x, y - imageHeight/2), ConnectionPoint.TYPE_FIGURE);
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x, y + imageHeight/2 + 5 + 14 ), ConnectionPoint.TYPE_FIGURE);
+    
+    f.finalise();
+    return f;
+}
+
+
+function figure_PDA(x, y){
+    /*As we do not know the size of the image (util it is loaded and is too late)
+     * we need to specify the size of it*/
+    var imageWidth = 36.66;
+    var imageHeight = 58.543;
+   
+    
+    var f = new Figure("PDA");
+    f.style.fillStyle = figure_defaultFillStyle;
+    f.style.strokeStyle = figure_defaultStrokeStyle;
+    
+    //Image
+    var url = "/editor/lib/sets/network/11_pda.svg";
+    
+    var ifig = new ImageFrame(url, x, y, true, imageWidth, imageHeight);
+    ifig.debug = true;
+    f.addPrimitive(ifig);    
+    
+    //Text
+    f.properties.push(new BuilderProperty('Text', 'primitives.1.str', BuilderProperty.TYPE_TEXT));
+    f.properties.push(new BuilderProperty('Text Size', 'primitives.1.size', BuilderProperty.TYPE_TEXT_FONT_SIZE));
+    f.properties.push(new BuilderProperty('Font', 'primitives.1.font', BuilderProperty.TYPE_TEXT_FONT_FAMILY));
+    f.properties.push(new BuilderProperty('Alignment', 'primitives.1.align', BuilderProperty.TYPE_TEXT_FONT_ALIGNMENT));
+    f.properties.push(new BuilderProperty('Text Color', 'primitives.1.style.fillStyle', BuilderProperty.TYPE_COLOR));
+    
+    var t2 = new Text(figure_defaultFigureTextStr, x, y + imageHeight/2 + 5, figure_defaultFigureTextFont, figure_defaultFigureTextSize);
+    t2.style.fillStyle = figure_defaultFillTextStyle;
+    f.addPrimitive(t2);
+    
+    //Connection Points
+    
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x + imageWidth/2, y), ConnectionPoint.TYPE_FIGURE);
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x - imageWidth/2, y), ConnectionPoint.TYPE_FIGURE);
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x, y - imageHeight/2), ConnectionPoint.TYPE_FIGURE);
+    CONNECTOR_MANAGER.connectionPointCreate(f.id, new Point(x, y + imageHeight/2 + 5 + 14 ), ConnectionPoint.TYPE_FIGURE);
+    
+    f.finalise();
+    return f;
+}
+
+
+function figure_Phone(x, y){
+    /*As we do not know the size of the image (util it is loaded and is too late)
+     * we need to specify the size of it*/
+    var imageWidth = 43.033;
+    var imageHeight = 37.081;
+   
+    
+    var f = new Figure("Phone");
+    f.style.fillStyle = figure_defaultFillStyle;
+    f.style.strokeStyle = figure_defaultStrokeStyle;
+    
+    //Image
+    var url = "/editor/lib/sets/network/12_phone.svg";
     
     var ifig = new ImageFrame(url, x, y, true, imageWidth, imageHeight);
     ifig.debug = true;
